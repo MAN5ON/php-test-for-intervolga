408a224cc6568823712dc10f6c852bed9d64717f
MAN5ON <dark34rus@mail.ru>
2023-05-19 02:56:07 +0300

    feat(global):add tasks to GITHUB

---
+++ task2/upload.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello World</title>
+</head>
+<body>
+<header class="header">
+    <h1>Hello There!</h1>
+</header>
+<main>
+    <article>
+        <form enctype="multipart/form-data" method="POST" action="formHandler.php">
+            <label class="inputFile">
+                <input class="file" type="file" name="file">
+                <span class="inputFileBtn">Choose File</span>
+            </label>
+            <button type="submit" class="submitButton">Download</button>
+        </form>
+
+    </article>
+    <article class="cats">
+        <h2>Chill Zone:</h2>
+        <section>
+            <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+                <figure>
+                    <img src="https://cdn.shopify.com/s/files/1/0344/6469/files/angry.jpg?v=1560891349"/>
+                    <figcaption>Mad Cat</figcaption>
+                </figure>
+                <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+                    <figure>
+                        <img
+                            src="https://img.huffingtonpost.com/asset/5d03615e240000300f91c3cb.jpeg?ops=scalefit_720_noupscale&format=webp"/>
+                        <figcaption>Chilly Chill</figcaption>
+                    </figure>
+                </a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+                    <figure>
+                        <img src="https://wallpapers-clan.com/wp-content/uploads/2022/07/funny-cat-21.jpg"/>
+                        <figcaption>Happy Happy Happy</figcaption>
+                    </figure>
+                </a>
+        </section>
+    </article>
+</main>
+<footer class="footer">
+    <p><a href="https://volgograd.hh.ru/resume/0c4166e4ff09aef6630039ed1f5273344b4732">Ссылка</a> на резюме
+    </p>
+</footer>
+</body>
+</html>
